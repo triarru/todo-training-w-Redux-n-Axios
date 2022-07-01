3a657c34f667fb2d870f46c60f2736293d9feb50
long <doduylong281200@gmail.com>
2022-07-01 15:20:23 +0700

    start doing todoList

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            React
+            TodoList
           </a>
           <span>, </span>
           <a
